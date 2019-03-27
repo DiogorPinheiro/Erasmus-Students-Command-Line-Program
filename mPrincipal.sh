9bdc63fe4dbbaf91933355d9c45fbc90d94255e1
Diogo Pinheiro <diogomrpinheiro@gmail.com>
2019-03-27 12:22:07 +0000

    inciar estatisticas

--- mPrincipal.sh
+++ mPrincipal.sh
@@ -9,12 +9,14 @@ echo "6) Estatísticas"
 echo "7) Quit"
 read opt
     case $opt in
-        1)
+        1)  
             echo $'\n'
             clear
             ./mRegisto.sh;;
-        2)  ./mAltera.sh
-            ;;
+        2)  
+            echo $'\n'
+            clear
+            ./mAltera.sh;;
         3)
             cat dados.txt
             echo $'\n\nPressione [ENTER] para avançar.\n'
@@ -31,7 +33,9 @@ read opt
             clear
             ./mBaseDeDados.sh;;
 
-        6) ;;
+        6)  echo $'\n'
+            clear
+            ./mEstatisticas.sh;;
 
         7)
             clear;;
