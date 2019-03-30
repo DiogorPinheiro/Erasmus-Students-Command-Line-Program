c804458a05cf58b95a844269745197a4f82c3fcf
Diogo Pinheiro <diogomrpinheiro@gmail.com>
2019-03-30 10:59:22 +0000

    por resolver

--- esProfessor.sh
+++ esProfessor.sh
@@ -4,7 +4,8 @@ dados="dados.txt"
 echo "1) NºProfessores"
 echo "2) Alunos por professor"
 echo "3) Média de alunos por professor"
-echo "4) Quit"
+echo "4) Professor com mais alunos"
+echo "5) Quit"
 read opt
     case $opt in
         1)  
@@ -48,7 +49,37 @@ read opt
             clear
             ./esProfessor.sh;;
         
-        4)  clear
+        4)
+            Arraytot=()
+            ArrayID=()
+            ID=40000
+            while ! grep -q $ID $dados;
+            do
+            
+                nome=$(grep $ID $dados| cut -d , -f 2)                              # Procurar linhas com esse valo na posição 2 do input de professores
+                echo "Nº de alunos associados ao professor(a) $nome"
+                valor=$(grep -o "$nome:" $dados |  wc -l)
+                ArrayID+=$ID
+                Arraytot+=$valor
+                ID=$(($ID+1))
+            done
+            echo $ID
+            pos=0
+            max=${Arraytot[0]}
+            for n in "${ArrayTot[@]}" ; do
+                if $n > $max 
+                then
+                    max=$n ;
+                    pos=n;
+                fi
+            done
+            echo $max
+            echo $'\n\nPressione [ENTER] para avançar.\n'                           
+            read rand
+            echo $'\n'
+            clear
+            ./esProfessor.sh;;        
+        5)  clear
             ./mEstatisticas.sh;;
         *) echo $'\n\nOpção inválida.\nPressione [ENTER] para avançar.\n'           # Evitar outros inputs não existentes no menu
             read rand
