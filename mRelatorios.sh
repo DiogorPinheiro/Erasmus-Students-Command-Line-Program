5ee943fbefd43b02d3b91c6d901260ae136e10a7
David Sales <salesdavid7@gmail.com>
2019-04-01 17:55:12 +0100

    dsoiafjas

--- mRelatorios.sh
+++ mRelatorios.sh
@@ -3,15 +3,14 @@ clear
 echo "1) Listar Universidades"
 echo "2) Listar Disciplinas"
 echo "3) Listar Alunos Erasmus"
-echo "4) Listar Disciplinas com Alunos Inscritos"
-echo "5) Listar Professores"
-echo "6) Sair para o Menu Principal"
+echo "4) Listar Professores"
+echo "5) Sair para o Menu Principal"
 read opt
     case $opt in
         1)
             clear
             echo 'Lista de Universidades:'
-            echo 'ID:Universidade:País'
+            echo $'\nID@Universidade@País\n'
             sed -n '/@/'p dados.txt
             echo $'\nPressione [ENTER] para avançar.\n'
             read rand
@@ -19,7 +18,8 @@ read opt
             ./mRelatorios.sh;;
         2)
             clear
-            echo Lista de Disciplinas:
+            echo 'Lista de Disciplinas:'
+            echo $'\nID#Nome#Semestre\n'
             sed -n '/#/'p dados.txt
             echo $'\nPressione [ENTER] para avançar.\n'
             read rand
@@ -27,23 +27,24 @@ read opt
             ./mRelatorios.sh;;
         3)
             clear
-            echo Lista de Alunos:
+            echo 'Lista de Alunos:'
+            echo $'\nID:Nome:Universidade:Professor:Semestre:Ano:Disciplinas\n'
             sed -n '/:/'p dados.txt
             echo $'\nPressione [ENTER] para avançar.\n'
             read rand
             echo $'\n'
             ./mRelatorios.sh;;
+        
         4)
-            ;;
-        5)
             clear
-            echo Lista de Professores:
+            echo 'Lista de Professores:'
+            echo $'\nID,Nome\n'
             sed -n '/,/'p dados.txt
             echo $'\nPressione [ENTER] para avançar.\n'
             read rand
             echo $'\n'
             ./mRelatorios.sh;;
-        6)
+        5)
             echo $'\n'
             ./mPrincipal.sh;;
         *) echo $'\n\nOpção inválida.\nPressione [ENTER] para avançar.\n'
