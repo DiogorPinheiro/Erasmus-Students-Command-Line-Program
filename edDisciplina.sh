b0de8f0e73bf09bd2555ab7cd436abe9d4034571
David Sales <salesdavid7@gmail.com>
2019-04-01 17:40:30 +0100

    fixed edDisciplina

--- edDisciplina.sh
+++ edDisciplina.sh
@@ -4,6 +4,7 @@ dados="dados.txt"
 cod=$1
 echo "1) Nome"
 echo "2) Semestre"
+echo "3) Sair"
 read opt
     case $opt in
 
@@ -15,26 +16,27 @@ read opt
                 echo $'Introduza um nome diferente:\n'
                 read nome
             done
-            sem=$(grep $cod $dados | cut -d '#' -f 3)
-            grep -v $cod $dados > tmp1.txt
-            echo "$cod#$nome#$sem" >> tmp1.txt
-            sort -n tmp1.txt > $dados
-            rm tmp1.txt
+            nomeAntigo=$(grep $1 $dados | cut -d '#' -f 2)
+            sed -i 's/'"#$nomeAntigo"'/'"#$nome"'/g' $dados #altera o nome da disciplina no registo da mesma
+            sed -i 's/'":$nomeAntigo"'/'":$nome"'/g' $dados #altera o nome da disciplina nos registos dos alunos
             ;;
 
         2)  
-            sem=$(grep $cod $dados | cut -d '#' -f 3)
+            sem=$(grep $cod $dados | cut -d '#' -f 3) #semestre atual
+            nome=$(grep $cod $dados | cut -d '#' -f 2) #procura o nome da mesma
             if [ $sem -eq 1 ]
             then
                 sem=2
+                sed -i 's/'"$nome#1"'/'"$nome#2"'/g' $dados #altera para o semestre contrário
             else
                 sem=1
+                sed -i 's/'"$nome#2"'/'"$nome#1"'/g' $dados #altera para o semestre contrário
             fi
-            nome=$(grep $cod $dados | cut -d '#' -f 2)
-            grep -v $cod $dados > tmp1.txt
-            echo "$cod#$nome#$sem" >> tmp1.txt
-            sort -n tmp1.txt > $dados
-            rm tmp1.txt
+            nome=$(grep $cod $dados | cut -d '#' -f 2) #procura o nome da mesma
+            sed -i 's/'":$nome"'/'""'/g' $dados #remove a mesma do registo do aluno, já que não está incluída nesse semestre
+            ;;
+
+        3)  exit
             ;;
 
         *) echo $'\n\nOpção inválida.\nPressione [ENTER] para avançar.\n'
